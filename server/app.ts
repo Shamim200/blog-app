8d7e646d7fed9b2e758b9683fb24ecb68e3f29c6
Shamim 200 <s40644812@gmail.com>
2026-01-14 13:13:18 +0600

    ✨feat(app): setup middleware for JSON and URL-encoded requests
    - Added middleware for parsing JSON and URL-encoded data.
    - Configured CORS with environment variable for origin.
    - Established user routes under /api/v1/users.

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,27 @@
 import express from "express";
+import cors from "cors";
+import userRouter from "./src/routes/user.route";
+const app = express();
 
-export const app = express();
+// middleware setup
+app.use(
+  express.json({
+    limit: "30kb",
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "30kb",
+  })
+);
+app.use(
+  cors({
+    origin: process.env.CORS,
+  })
+);
+
+// routes setup
+app.use("/api/v1/users", userRouter);
+
+export { app };
