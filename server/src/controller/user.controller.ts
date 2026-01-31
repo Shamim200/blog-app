979d92c518824c40eec0c093332ac9ce928d093b
Shamim 200 <s40644812@gmail.com>
2026-01-31 20:57:38 +0600

    ✨ feat(controller):  create user related controller
    -Add user register , login, logout and user profile controller

---
+++ server/src/controller/user.controller.ts
@@ -0,0 +1,8 @@
+import { AsyncHandler } from "../utils/AsyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+
+export const userRegister = AsyncHandler(async (req, res) => {});
+export const userLogin = AsyncHandler(async (req, res) => {});
+export const userLoggout = AsyncHandler(async (req, res) => {});
+export const userProfile = AsyncHandler(async (req, res) => {});
