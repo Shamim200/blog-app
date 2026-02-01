e58e29a9887ce240385279a71f1c59e29b8070e0
Shamim 200 <s40644812@gmail.com>
2026-02-01 20:23:56 +0600

    ✨feat(routes): Implement user registration, login, logout, and profile routes using user controller functions

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import {
+  userRegister,
+  userLogin,
+  userLoggout,
+  userProfile,
+} from "../controller/user.controller";
 
 const router = Router();
 
-router.route("/register").post();
-router.route("/login").post();
-router.route("/logout").post();
-router.route("/profile").get();
+router.route("/register").post(userRegister);
+router.route("/login").post(userLogin);
+router.route("/logout").post(userLoggout);
+router.route("/profile").get(userProfile);
 
 export default router;
